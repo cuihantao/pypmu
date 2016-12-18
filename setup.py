418dc7bf08e641f1d3feb2cc52a02d24550daadb
Hantao Cui <hcui7@cepsltb4.eecs.utk.edu>
2016-12-17 22:30:31 -0500

    Simple setup.py script to avoid adding env vars

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='pypmu',
+      version='0.1.1',
+      description='Python-based PMU simulator',
+      author='Stevan Sandi',
+      url='https://github.com/cuihantao/pypmu',
+      packages=['synchrophasor'],
+      )
+
+
+
+
