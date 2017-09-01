15955b23900846025baf3fadb023a7b66c3956de
Austin <rmcever@vols.utk.edu>
2017-09-01 15:21:18 -0400

    added comments

--- examples/uniquer.py
+++ examples/uniquer.py
@@ -1,5 +1,6 @@
 """This python 3 script searches a .lst file and creates a duplicate file where
-each bus name is unique. File name is currently hard coded
+each bus name is unique and devoid of characters that openPDC cannot use.
+File names currently hard coded
 """
 import re
 
@@ -9,17 +10,17 @@ import re
 spec = r'[&,-]+' # include any special characters, e.g. '&' and '-' that should be removed from bus names
 
 name_counts = {} # hold each bus name and its count
-f = open("WECC_OSI_out.lst", "r+")
+f = open("WECC_OSI_out.lst", "r")
 fd = open("WECC_OSI_out-cor.lst", "w") # corrected file
 for line in f:
     if "$\\theta\\" in line:
         # get bus name
         fields = line.split(",")
         theta_name = fields[1].strip()
-        name = theta_name[6:]
+        name = theta_name[6:] # remove "theta "
         if re.search(spec, name): # if name contains invalid characters
             new_name = re.sub(spec, "", name) # remove special characters in spec from name
-            line = re.sub(name, new_name, line)
+            line = re.sub(name, new_name, line) # correct the line
             name = new_name
         if name in name_counts:
             line = re.sub(name, name + "_" + str(name_counts[name]), line) # replace name
