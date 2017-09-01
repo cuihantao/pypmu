0679ff97ca9c3bdd16ac04fe34e13126048037c4
Austin <rmcever@vols.utk.edu>
2017-09-01 13:46:37 -0400

    Added code to remove special characters and ran it

--- examples/uniquer.py
+++ examples/uniquer.py
@@ -3,6 +3,11 @@ each bus name is unique. File name is currently hard coded
 """
 import re
 
+# make sure to escape characters when required by re module
+# done this way because I'm not sure which special characters are not allowed
+# by openPDC, and may need to add more later
+spec = r'[&,-]+' # include any special characters, e.g. '&' and '-' that should be removed from bus names
+
 name_counts = {} # hold each bus name and its count
 f = open("WECC_OSI_out.lst", "r+")
 fd = open("WECC_OSI_out-cor.lst", "w") # corrected file
@@ -12,8 +17,12 @@ for line in f:
         fields = line.split(",")
         theta_name = fields[1].strip()
         name = theta_name[6:]
+        if re.search(spec, name): # if name contains invalid characters
+            new_name = re.sub(spec, "", name) # remove special characters in spec from name
+            line = re.sub(name, new_name, line)
+            name = new_name
         if name in name_counts:
-            line = re.sub(name, name + "_" + str(name_counts[name]), line) # replace name 
+            line = re.sub(name, name + "_" + str(name_counts[name]), line) # replace name
             name_counts[name] += 1
         else:
             name_counts[name] = 1
