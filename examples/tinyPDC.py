a524a3007d34b8abb74e908f3fa05d1bcf6fe9e8
Jinyoung Lee <jlee219@vols.utk.edu>
2018-09-27 15:40:21 -0400

    adding support for multiple PMUs

--- examples/tinyPDC.py
+++ examples/tinyPDC.py
@@ -1,22 +1,76 @@
 from pypmu.pdc import Pdc
-
+from pypmu.frame import DataFrame
+from multiprocessing import Process, Pipe, Queue
+from multiprocessing import Pool
+#from queue import Queue
+import multiprocessing
 """
 tinyPDC will connect to pmu_ip:pmu_port and send request
 for header message, configuration and eventually
 to start sending measurements.
 """
+iplist=[]
+iplist.append('127.0.0.1')
+iplist.append('127.0.0.2')
+iplist.append('127.0.0.3')
+iplist.append('127.0.0.4')
+iplist.append('127.0.0.5')
+iplist.append('127.0.0.6')
+iplist.append('127.0.0.7')
+iplist.append('127.0.0.8')
+pmunumber = 8
+if __name__ == "__main__":
+    pdc1 = Pdc(pdc_id=7, pmu_ip=iplist[0], pmu_port=1410)
+    pdc2 = Pdc(pdc_id=7, pmu_ip=iplist[1], pmu_port=1410)
+    pdc3 = Pdc(pdc_id=7, pmu_ip=iplist[2], pmu_port=1410)
+#    pdc4 = Pdc(pdc_id=7, pmu_ip=iplist[3], pmu_port=1410)
+    pdc1.logger.setLevel("DEBUG")
+    pdc2.logger.setLevel("DEBUG")
+    pdc3.logger.setLevel("DEBUG")
+#    pdc4.logger.setLevel("DEBUG")
+    pdc1.run()  # Connect to PMU
+    pdc2.run()  # Connect to PMU
+    pdc3.run()  # Connect to PMU
+#    pdc4.run()  # Connect to PMU
+    header = pdc1.get_header()  # Get header message from PMU
+    config = pdc1.get_config()  # Get configuration from PMU
+    header = pdc2.get_header()  # Get header message from PMU
+    config = pdc2.get_config()
+    header = pdc3.get_header()  # Get header message from PMU
+    config = pdc3.get_config()
+    
+    pdc1.start()  # Request to start sending measurements
+    pdc2.start()  # Request to start sending measurements
+    pdc3.start()  # Request to start sending measurements
 
-pdc = Pdc(pdc_id=7, pmu_ip='127.0.0.1', pmu_port=1414)
+    while True:
 
-pdc.run()  # Connect to PMU
+        Test=[]
 
-header = pdc.get_header()  # Get header message from PMU
-config = pdc.get_config()  # Get configuration from PMU
+        result_queue=Queue()
+        result_queue1=Queue()
+        result_queue2=Queue()
 
-pdc.start()  # Request to start sending measurements
+        data1=Process(target=pdc1.getmsg, args=(result_queue,))
 
-while True:
-    data = pdc.get()  # Keep receiving data
-    if not data:
-        pdc.quit()  # Close connection
-        break
+        data1.start()
+        results= result_queue.get()
+        if type(results)==DataFrame:
+            print (results.get_measurements())
+        data1.terminate()
+
+        data2=Process(target=pdc2.getmsg, args=(result_queue1,))
+        data2.start()
+        results1= result_queue1.get()
+        if type(results1)==DataFrame:
+            print (results1.get_measurements())
+        data2.terminate()
+
+        data3=Process(target=pdc3.getmsg, args=(result_queue2,))
+        data3.start()
+        results2= result_queue2.get()
+        if type(results2)==DataFrame:
+            print (results2.get_measurements())
+        data3.terminate()
+
+
--- pypmu/pdc.py
+++ pypmu/pdc.py
@@ -1,28 +1,28 @@
 import logging
 import socket
-
 from sys import stdout
 from pypmu.frame import *
-
+from multiprocessing import Queue
+#from queue import Queue
+#import queue
 __author__ = "Stevan Sandi"
 __copyright__ = "Copyright (c) 2016, Tomo Popovic, Stevan Sandi, Bozo Krstajic"
 __credits__ = []
 __license__ = "BSD-3"
-__version__ = "0.1"
-
+__version__ = "1.0.0-alpha"
 
 class Pdc(object):
 
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
 
-    # handler = logging.StreamHandler(stdout)
-    # handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-    # handler.setFormatter(formatter)
-    # logger.addHandler(handler)
+    handler = logging.StreamHandler(stdout)
+    formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
-    def __init__(self, pdc_id=1, pmu_ip='127.0.0.1', pmu_port=4712, buffer_size=2048, method='tcp'):
+
+    def __init__(self, pdc_id=1, pmu_ip="127.0.0.1", pmu_port=4712, buffer_size=2048, method="tcp"):
 
         self.pdc_id = pdc_id
         self.buffer_size = buffer_size
@@ -32,6 +32,10 @@ class Pdc(object):
         self.pmu_port = pmu_port
         self.pmu_address = (pmu_ip, pmu_port)
         self.pmu_socket = None
+        self.pmu_cfg1 = None
+        self.pmu_cfg2 = None
+        self.pmu_header = None
+
 
     def run(self):
 
@@ -48,30 +52,33 @@ class Pdc(object):
                 self.logger.error("[%d] - Error while connecting to (%s:%d)", self.pdc_id, self.pmu_ip, self.pmu_port)
                 self.logger.error(str(e))
 
+
     def start(self):
         """
         Request from PMU to start sending data
         :return: NoneType
         """
-        start = CommandFrame(self.pdc_id, 'start')
+        start = CommandFrame(self.pdc_id, "start")
         self.pmu_socket.sendall(start.convert2bytes())
         self.logger.info("[%d] - Requesting to start sending from PMU (%s:%d)", self.pdc_id, self.pmu_ip, self.pmu_port)
 
+
     def stop(self):
         """
-        Request from PMU to stop sending data
+        Request from PMU to start sending data
         :return: NoneType
         """
-        start = CommandFrame(self.pdc_id, 'stop')
+        start = CommandFrame(self.pdc_id, "stop")
         self.pmu_socket.sendall(start.convert2bytes())
         self.logger.info("[%d] - Requesting to stop sending from PMU (%s:%d)", self.pdc_id, self.pmu_ip, self.pmu_port)
+
 
     def get_header(self):
         """
         Request for PMU header message
         :return: HeaderFrame
         """
-        get_header = CommandFrame(self.pdc_id, 'header')
+        get_header = CommandFrame(self.pdc_id, "header")
         self.pmu_socket.sendall(get_header.convert2bytes())
 
         self.logger.info("[%d] - Requesting header message from PMU (%s:%d)", self.pdc_id, self.pmu_ip, self.pmu_port)
@@ -80,21 +87,28 @@ class Pdc(object):
         if isinstance(header, HeaderFrame):
             return header
         else:
-            return None
-    def get_config(self, version='cfg2'):
+            raise PdcError("Invalid Header message received")
+
+
+    def get_config(self, version="cfg2"):
         """
         Request for Configuration frame.
-        :param version: string Possible values 'cfg1', 'cfg2' and 'cfg3'
+        :param version: string Possible values "cfg1", "cfg2" and "cfg3"
         :return: ConfigFrame
         """
         get_config = CommandFrame(self.pdc_id, version)
         self.pmu_socket.sendall(get_config.convert2bytes())
 
         config = self.get()
-        if isinstance(config, ConfigFrame):
-            return config
+        if type(config) == ConfigFrame1:
+            self.pmu_cfg1 = config
+        elif type(config) == ConfigFrame2:
+            self.pmu_cfg2 = config
         else:
-            return None
+            raise PdcError("Invalid Configuration message received")
+
+        return config
+
 
     def get(self):
         """
@@ -102,7 +116,7 @@ class Pdc(object):
         :return: CommonFrame
         """
 
-        received_data = b''
+        received_data = b""
         received_message = None
 
         """
@@ -115,20 +129,20 @@ class Pdc(object):
             received_data += self.pmu_socket.recv(self.buffer_size)
 
         bytes_received = len(received_data)
-        total_frame_size = pmu_code = int.from_bytes(received_data[2:4], byteorder='big', signed=False)
+        total_frame_size = int.from_bytes(received_data[2:4], byteorder="big", signed=False)
 
         # Keep receiving until every byte of that message is received
         while bytes_received < total_frame_size:
             message_chunk = self.pmu_socket.recv(min(total_frame_size - bytes_received, self.buffer_size))
             if not message_chunk:
                 break
-            received_data.append(message_chunk)
+            received_data += message_chunk
             bytes_received += len(message_chunk)
 
         # If complete message is received try to decode it
         if len(received_data) == total_frame_size:
             try:
-                received_message = CommonFrame.convert2frame(received_data)  # Try to decode received data
+                received_message = CommonFrame.convert2frame(received_data, self.pmu_cfg2)  # Try to decode received data
                 self.logger.debug("[%d] - Received %s from PMU (%s:%d)", self.pdc_id, type(received_message).__name__,
                                   self.pmu_ip, self.pmu_port)
             except FrameError:
@@ -136,6 +150,47 @@ class Pdc(object):
                                     self.pdc_id, self.pmu_ip, self.pmu_port)
 
         return received_message
+
+    def getmsg(self,queue):
+        #Print (queue)
+        #print (self)
+        received_data = b""
+        received_message = None
+
+        while len(received_data) < 4:
+            received_data += self.pmu_socket.recv(self.buffer_size)
+
+        bytes_received = len(received_data)
+        total_frame_size = int.from_bytes(received_data[2:4], byteorder="big", signed=False)
+
+        # Keep receiving until every byte of that message is received
+        while bytes_received < total_frame_size:
+            message_chunk = self.pmu_socket.recv(min(total_frame_size - bytes_received, self.buffer_size))
+
+            if not message_chunk:
+                break
+            received_data += message_chunk
+            bytes_received += len(message_chunk)
+
+        # If complete message is received try to decode it
+        if len(received_data) == total_frame_size:
+            try:
+                received_message = CommonFrame.convert2frame(received_data, self.pmu_cfg2)  # Try to decode received data
+                self.logger.debug("[%d] - Received %s from PMU (%s:%d)", self.pdc_id, type(received_message).__name__,
+                                  self.pmu_ip, self.pmu_port)
+            except FrameError:
+                self.logger.warning("[%d] - Received unknown message from PMU (%s:%d)",
+                                    self.pdc_id, self.pmu_ip, self.pmu_port)
+
+        #Queuepush(queue,received_message)
+        #print ('Before Queue put')
+        queue.put(received_message)
+        #print ('After Queue put')
+        #print (queue.get())
+        #print (received_message)
+        return received_message
+        #return queue
+
 
     def quit(self):
         """
